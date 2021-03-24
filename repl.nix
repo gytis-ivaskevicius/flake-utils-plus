4c7a9a976520253568e9f79551773055eeed0613
Gytis Ivaskevicius <me@gytis.io>
2021-03-24 22:41:03 +0200

    Yep, I just made repl even better. Pkgs supports overlays out of the box :)

--- repl.nix
+++ repl.nix
@@ -1,7 +1,7 @@
 let
   flake = builtins.getFlake "flake:self";
   hostname = builtins.head (builtins.match "([a-zA-Z0-9]+)\n" (builtins.readFile "/etc/hostname"));
-  nixpkgs = import flake.inputs.nixpkgs { };
+  nixpkgs = flake.pkgs.${builtins.currentSystem}.nixpkgs;
 in
 { inherit flake; }
 // flake
