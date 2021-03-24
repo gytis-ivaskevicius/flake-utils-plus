c9fc0e2fe2906edc1021a81c371fb1ab856e3f8b
Gytis Ivaskevicius <me@gytis.io>
2021-03-24 14:32:13 +0200

    Fix #5 - Remove custom NIX_PATH implementation

--- modules/saneFlakeDefaults.nix
+++ modules/saneFlakeDefaults.nix
@@ -3,10 +3,6 @@
 let
   flakes = lib.filterAttrs (name: value: value ? outputs) inputs;
 
-  nixPath = lib.mapAttrsToList
-    (name: _: "${name}=${inputs.${name}}")
-    flakes;
-
   nixRegistry = builtins.mapAttrs
     (name: v: { flake = v; })
     flakes;
@@ -14,7 +10,6 @@ in {
 
   nix =  {
     extraOptions = "experimental-features = nix-command ca-references flakes";
-    nixPath = nixPath;
     registry = nixRegistry;
     package = pkgs.nixUnstable;
   };
