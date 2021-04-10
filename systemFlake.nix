ddcd703a0bbf478bc374a081c41975d4677897c2
Pacman99 <pachum99@gmail.com>
2021-04-09 19:02:37 -0700

    allow custom config system builder and output

--- systemFlake.nix
+++ systemFlake.nix
@@ -29,28 +29,42 @@ let
   inherit (flake-utils-plus.lib) eachSystem;
 
   # ensure for that all expected, but no extra attrs are present
-  validateHost = {
-      channelName ? null
+  validateHost =
+    { channelName ? null
     , system ? null
-    , modules ? []
-    , extraArgs ? {}
-  }: { inherit channelName system modules extraArgs; };
+    , output ? null
+    , builder ? null
+    , modules ? [ ]
+    , extraArgs ? { }
+    }: { inherit channelName system output builder modules extraArgs; };
 
   mergeHosts = lhs: rhs:
-  let
-    # convoluted nix-kell, but clean(er) english-german below :-)
-    _ = x: op: y: op x y;
-    oder = lhs: rhs: if lhs != null then lhs else rhs;
+    let
+      # convoluted nix-kell, but clean(er) english-german below :-)
+      _ = x: op: y: op x y;
+      oder = lhs: rhs: if lhs != null then lhs else rhs;
 
-    rhs' = { channelName = _ rhs.channelName; system = _ rhs.system; };
-    lhs' = { channelName = _ lhs.channelName; system = _ lhs.system; };
-  in 
-  {
-    channelName = rhs'.channelName oder (lhs'.channelName oder "nixpkgs") ;
-    system = rhs'.system oder (lhs'.system oder defaultSystem) ; # replace deaultSystem with x86_64-linux
-    modules = rhs.modules ++ lhs.modules ++ sharedModules;
-    extraArgs = sharedExtraArgs // lhs.extraArgs // rhs.extraArgs;
-  };
+      rhs' = {
+        channelName = _ rhs.channelName;
+        system = _ rhs.system;
+        output = _ rhs.output;
+        builder = _ rhs.builder;
+      };
+      lhs' = {
+        channelName = _ lhs.channelName;
+        system = _ lhs.system;
+        output = _ lhs.output;
+        builder = _ lhs.builder;
+      };
+    in
+    rec {
+      channelName = rhs'.channelName oder (lhs'.channelName oder "nixpkgs");
+      system = rhs'.system oder (lhs'.system oder defaultSystem); # replace deaultSystem with x86_64-linux
+      output = rhs'.output oder (lhs'.output oder "nixosConfigurations");
+      builder = rhs'.builder oder (lhs'.builder oder channels.${channelName}.input.lib.nixosSystem);
+      modules = rhs.modules ++ lhs.modules ++ sharedModules;
+      extraArgs = sharedExtraArgs // lhs.extraArgs // rhs.extraArgs;
+    };
 
   optionalAttrs = check: value: if check then value else { };
 
@@ -58,7 +72,7 @@ let
     "defaultSystem" # TODO: deprecated, remove
     "sharedExtraArgs"
     "inputs"
-    "nixosHosts"
+    "hosts"
     "hostDefaults"
     "channels"
     "channelsConfig"
@@ -75,50 +89,50 @@ let
     "checksBuilder"
   ];
 
-  nixosConfigurationBuilder = hostname: host:
-    let
-      host' =
-        mergeHosts (validateHost hostDefaults) (validateHost host);
-    in
-      # It would be nice to get `nixosSystem` reference from `selectedNixpkgs` but it is not possible at this moment
-      inputs."${host'.channelName}".lib.nixosSystem
-        (genericConfigurationBuilder hostname host');
-
   getNixpkgs = host: self.pkgs."${host.system}"."${host.channelName}";
 
-  genericConfigurationBuilder = hostname: host: (
-    let selectedNixpkgs = getNixpkgs host; in
+  configurationBuilder = hostname: host': (
+    let
+      selectedNixpkgs = getNixpkgs host;
+      host = mergeHosts (validateHost hostDefaults) (validateHost host');
+    in
     {
-      inherit (selectedNixpkgs) system;
-      modules = [
-        ({ pkgs, lib, options, ... }: {
-          # 'mkMerge` to separate out each part into its own module
-          _type = "merge";
-          contents = [
-            (optionalAttrs (options ? networking.hostName) { 
-              networking.hostName = hostname; 
-            })
+      name = host.output;
+      value.${hostname} = host.builder {
+        inherit (selectedNixpkgs) system;
+        modules = [
+          ({ pkgs, lib, options, ... }: {
+            # 'mkMerge` to separate out each part into its own module
+            _type = "merge";
+            contents = [
+              (optionalAttrs (options ? networking.hostName) {
+                networking.hostName = hostname;
+              })
 
-            (if options ? nixpkgs then {
-              nixpkgs = {
-                inherit (selectedNixpkgs) overlays config system;
-              };
-            } else { _module.args.pkgs = selectedNixpkgs; })
+              (if options ? nixpkgs then {
+                nixpkgs = {
+                  inherit (selectedNixpkgs) overlays config system;
+                };
+              } else { _module.args.pkgs = selectedNixpkgs; })
 
-            (optionalAttrs (options ? system.configurationRevision) {
-              system.configurationRevision = lib.mkIf (self ? rev) self.rev;
-            })
+              (optionalAttrs (options ? system.configurationRevision) {
+                system.configurationRevision = lib.mkIf (self ? rev) self.rev;
+              })
 
-            (optionalAttrs (options ? nix.package) { 
-              nix.package = lib.mkDefault pkgs.nixUnstable;
-            })
-          ];
-        })
-      ]
-      ++ host.modules;
-      extraArgs = { inherit inputs; } // host.extraArgs;
+              (optionalAttrs (options ? nix.package) {
+                nix.package = lib.mkDefault pkgs.nixUnstable;
+              })
+            ];
+          })
+        ] ++ host.modules;
+        extraArgs = { inherit inputs; } // host.extraArgs;
+      };
     }
   );
+
+  mapAttrs' = f: set:
+    builtins.listToAttrs (map (attr: f attr set.${attr}) (builtins.attrNames set));
+
 in
 otherArguments
 
@@ -150,8 +164,7 @@ otherArguments
   // optional devShellBuilder { devShell = devShellBuilder pkgs; }
   // optional checksBuilder { checks = checksBuilder pkgs; }
 )
-
-  // {
-  nixosConfigurations = nixosConfigurations // (builtins.mapAttrs nixosConfigurationBuilder nixosHosts);
-}
-
+  # produces attrset in the shape of
+  # { nixosConfigurations = {}; darwinConfigurations = {};  ... } 
+  # according to profile.output or the default `nixosConfigurations`
+  // mapAttrs' configurationBuilder hosts
