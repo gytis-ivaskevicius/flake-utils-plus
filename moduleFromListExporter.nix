431f9bd92163702be11d188f217870cb6fb5d1d3
David Arnold <dar@xoe.solutions>
2021-04-14 11:26:51 -0500

    docs: modulesFromListExporter

--- moduleFromListExporter.nix
+++ moduleFromListExporter.nix
@@ -1,29 +1,47 @@
 { flake-utils-plus }: let
 
-modulesFromListExporter =
+  modulesFromListExporter = paths:
+    /**
+    Synopsis: modulesFromListExporter _paths_
 
-  let
+    paths:    [ <path> <path> ]
 
-    removeSuffix = suffix: str:
-      let
-        sufLen = builtins.stringLength suffix;
-        sLen = builtins.stringLength str;
-      in
-      if sufLen <= sLen && suffix == builtins.substring (sLen - sufLen) sufLen str then
-        builtins.substring 0 (sLen - sufLen) str
-      else
-        str;
+    Returns an attribute set of modules from a list of paths by converting
+    the path's basename into the attribute key.
 
-    genAttrs' = func: values: builtins.listToAttrs (map func values);
+    Example:
 
-  in
+    paths:    [ ./path/to/moduleA.nix ./path/to/moduleBfolder ]
 
-    paths: genAttrs'
-      (path: {
-        name = removeSuffix ".nix" (baseNameOf path);
-        value = import path;
-      })
-      paths;
+    {
+      moduleA = import ./path/to/moduleA.nix;
+      moduleBfolder = import ./path/to/moduleBfolder;
+    }
+
+    **/
+
+    let
+
+      removeSuffix = suffix: str:
+        let
+          sufLen = builtins.stringLength suffix;
+          sLen = builtins.stringLength str;
+        in
+        if sufLen <= sLen && suffix == builtins.substring (sLen - sufLen) sufLen str then
+          builtins.substring 0 (sLen - sufLen) str
+        else
+          str;
+
+      genAttrs' = func: values: builtins.listToAttrs (map func values);
+
+    in
+
+      genAttrs'
+        (path: {
+          name = removeSuffix ".nix" (baseNameOf path);
+          value = import path;
+        })
+        paths;
 
 in
 modulesFromListExporter
