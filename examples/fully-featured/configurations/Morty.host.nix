7418d2ca5adff110c13defcab26cd349d0f3cea8
David Arnold <dar@xoe.solutions>
2021-04-21 23:40:24 -0500

    imp: provide per channel modules paths to make it easy to "backport"
    a module. imports = [ ${myChannelModulesPathk}/installer/...
    
    Signed-off-by: David Arnold <dar@xoe.solutions>

--- examples/fully-featured/configurations/Morty.host.nix
+++ examples/fully-featured/configurations/Morty.host.nix
@@ -1,4 +1,8 @@
-{ ... }: {
+# auto-special args <channelName>ModulesPath for easy backporting of modules
+{ unstableModulesPath, ... }: {
+
+  imports = [ "${unstableModulesPath}/installer/cd-dvd/installation-cd-minimal-new-kernel.nix" ];
+  disabledModules = [ "installer/cd-dvd/installation-cd-minimal-new-kernel.nix" ];
+
   boot.loader.grub.devices = [ "nodev" ];
-  fileSystems."/" = { device = "test"; fsType = "ext4"; };
 }
