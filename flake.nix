8eb7f9206713a528174c20c5133521dc37e2bfb1
Pacman99 <pachum99@gmail.com>
2021-04-26 23:45:51 -0700

    export mergeAny in public lib

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
 
   outputs = { self, flake-utils }:
     let
+      inherit (builtins) isList isAttrs mapAttrs;
       fupArgs = { flake-utils-plus = self; };
       systemFlake = import ./systemFlake.nix fupArgs;
       packagesFromOverlaysBuilderConstructor = import ./packagesFromOverlaysBuilderConstructor.nix fupArgs;
@@ -30,6 +31,16 @@
         };
 
         repl = ./repl.nix;
+
+        # merge nested attribute sets and lists
+        mergeAny = lhs: rhs:
+          lhs // mapAttrs
+            (name: value:
+              if isAttrs value then lhs.${name} or { } // value
+              else if isList value then lhs.${name} or [ ] ++ value
+              else value
+            )
+            rhs;
 
         patchChannel = system: channel: patches:
           if patches == [ ] then channel else
