5d36f03c3697d07a0c43adeace8b79474473c498
Gytis Ivaskevicius <me@gytis.io>
2021-05-23 20:06:05 +0300

    Work on repl.nix

--- lib/repl.nix
+++ lib/repl.nix
@@ -1,12 +1,18 @@
+{ flakePath ? "flake:self", hostnamePath ? "/etc/hostname" }:
+
 let
-  inherit (builtins) getFlake head match currentSystem;
-  flake = getFlake "flake:self";
-  hostname = head (match "([a-zA-Z0-9]+)\n" (builtins.readFile "/etc/hostname"));
-  nixpkgs = flake.pkgs.${currentSystem}.nixpkgs;
+  inherit (builtins) getFlake head match currentSystem readFile pathExists;
+  flake = getFlake (toString flakePath);
+  hostname = if pathExists hostnamePath then head (match "([a-zA-Z0-9]+)\n" (readFile hostnamePath)) else "";
+
+  nixpkgsFromInputsPath = flake.inputs.nixpkgs.outPath or "";
+  nixpkgs = flake.pkgs.${currentSystem}.nixpkgs or (if nixpkgsFromInputsPath != "" then import nixpkgsFromInputsPath { } else { });
+
+  nixpkgsOutput = (removeAttrs (nixpkgs // nixpkgs.lib or { }) [ "options" "config" ]);
 in
 { inherit flake; }
 // flake
 // builtins
-// flake.nixosConfigurations.${hostname} or { }
-// (removeAttrs nixpkgs.lib [ "options" ])
-  // (removeAttrs nixpkgs [ "config" ])
+// (flake.nixosConfigurations or { })
+// flake.nixosConfigurations.${builtins.trace hostname hostname} or { }
+  // nixpkgsOutput
