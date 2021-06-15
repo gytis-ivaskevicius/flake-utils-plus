9608ba9742a6e021935bb82985b1db16b62d762d
David Arnold <dar@xoe.solutions>
2021-06-14 21:21:18 -0500

    modulesFromList: infer names from paths or modules's _file attr

--- lib/modulesFromList.nix
+++ lib/modulesFromList.nix
@@ -1,22 +1,22 @@
 { flake-utils-plus }:
 let
 
-  modulesFromListExporter = paths:
+  modulesFromListExporter = args:
     /**
-      Synopsis: modulesFromListExporter _paths_
+      Synopsis: modulesFromListExporter _paths or modules_
 
-      paths:    [ <path> <path> ]
+      paths:    [ <path> <module> ]
 
-      Returns an attribute set of modules from a list of paths by converting
-      the path's basename into the attribute key.
+      Returns an attribute set of modules from a list of paths or modules by converting
+      the path's basename / the module's _file attribute's basename into the attribute key.
 
       Example:
 
-      paths:    [ ./path/to/moduleA.nix ./path/to/moduleBfolder ]
+      paths:    [ ./path/to/moduleA.nix { _file = ./path/to/moduleBfolder; ... } ]
 
       {
       moduleA = import ./path/to/moduleA.nix;
-      moduleBfolder = import ./path/to/moduleBfolder;
+      moduleBfolder = { _file = ./path/to/moduleBfolder; ... }
       }
 
       **/
@@ -35,14 +35,40 @@ let
 
       genAttrs' = func: values: builtins.listToAttrs (map func values);
 
+      hasFileAttr = o: builtins.hasAttr "_file" o;
+      peek = f: f (builtins.functionArgs f);
+
     in
 
     genAttrs'
-      (path: {
-        name = removeSuffix ".nix" (baseNameOf path);
-        value = import path;
-      })
-      paths;
+      (arg:
+
+        # a regular path to be imported
+        if builtins.isPath arg then
+          {
+            name = removeSuffix ".nix" (baseNameOf arg);
+            value = import arg;
+          }
+
+        # a module function with a _file attr
+        else if ((builtins.isFunction arg) && (hasFileAttr (peek args))) then
+          {
+            name = removeSuffix ".toml" (removeSuffix ".nix" (baseNameOf (peek arg)._file));
+            value = arg;
+          }
+
+        # a module with a _file attr
+        else if (hasFileAttr arg) then
+          {
+            name = removeSuffix ".toml" (removeSuffix ".nix" (baseNameOf arg._file));
+            value = arg;
+          }
+
+        # panic: something else
+        else
+          builtins.throw "either pass a path or a module with _file key to modulesFromListExporter"
+      )
+      args;
 
 in
 modulesFromListExporter
