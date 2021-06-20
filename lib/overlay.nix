f60eb469be1a759bce1ee2a6c820efdfb224658c
Gytis Ivaskevicius <me@gytis.io>
2021-06-20 15:17:25 +0300

    Seperate repl overlay and implement help menu

---
+++ lib/overlay.nix
@@ -0,0 +1,25 @@
+final: prev:
+let
+  example = command: desc: ''\n\u001b[33m ${command}\u001b[0m - ${desc}'';
+in
+{
+  __dontExport = true;
+
+  fup-repl = final.writeShellScriptBin "repl" ''
+    case "$1" in
+      "-h"|"--help"|"help")
+        printf "%b\n\e[4mUsage\e[0m: \
+          ${example "repl" "Loads system flake if available."} \
+          ${example "repl /path/to/flake.nix" "Loads specified flake."}\n"
+      ;;
+      *)
+        if [ -z "$1" ]; then
+          nix repl ${./repl.nix}
+        else
+          nix repl --arg flakePath $(readlink -f $1 | sed 's|/flake.nix||') ${./repl.nix}
+        fi
+      ;;
+    esac
+  '';
+}
+
