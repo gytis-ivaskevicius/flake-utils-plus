91cad97a01a76c4cc3c218a4ce1fa6408ec11616
Gytis Ivaskevicius <me@gytis.io>
2021-06-20 15:47:05 +0300

    Fix repl paths checking

--- lib/repl.nix
+++ lib/repl.nix
@@ -2,10 +2,14 @@
 
 let
   inherit (builtins) getFlake head match currentSystem readFile pathExists filter fromJSON;
+  selfFlake = filter (it: it.from.id == "self") (fromJSON (readFile /etc/nix/registry.json)).flakes;
   flakePath' =
     if flakePath != null
     then flakePath
-    else (head (filter (it: it.from.id == "self") (fromJSON (readFile /etc/nix/registry.json)).flakes)).to.path;
+    else if selfFlake != []
+    then (head selfFlake).to.path
+    else "/etc/nixos";
+
   flake = getFlake (toString flakePath');
   hostname = if pathExists hostnamePath then head (match "([a-zA-Z0-9]+)\n" (readFile hostnamePath)) else "";
 
