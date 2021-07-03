0a8e4005a3abc840eded875b302a605a4d422298
Gytis Ivaskevicius <me@gytis.io>
2021-07-03 17:59:47 +0300

    Add mkDefault to extra-options

--- lib/systemFlake.nix
+++ lib/systemFlake.nix
@@ -196,7 +196,7 @@ let
               })
 
               (optionalAttrs (options ? nix.extraOptions) {
-                nix.extraOptions = "experimental-features = nix-command ca-references flakes";
+                nix.extraOptions = lib.mkDefault "experimental-features = nix-command ca-references flakes";
               })
 
               {
