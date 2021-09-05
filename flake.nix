08d7921714df8c93b5aec6b6a142f93cc454abe0
Gytis Ivaskevicius <me@gytis.io>
2021-09-05 18:47:12 +0300

    Remove deprecated code

--- flake.nix
+++ flake.nix
@@ -24,20 +24,12 @@
     rec {
       inherit overlay;
 
-      # Deprecated in favor of 'nix.generateRegistryFromInputs = true;'
-      nixosModules.saneFlakeDefaults = { nix.generateRegistryFromInputs = true; };
       nixosModules.autoGenFromInputs = import ./lib/options.nix;
 
       devShell.x86_64-linux = import ./devShell.nix { system = "x86_64-linux"; };
 
       lib = flake-utils.lib // {
-        inherit mkFlake exportModules exportOverlays exportPackages;
-
-        # Deprecated - should use top-level functions instead
-        exporters = {
-          inherit modulesFromList fromOverlays internalOverlays;
-        };
-        inherit systemFlake modulesFromList;
+        inherit mkFlake exportModules exportOverlays exportPackages systemFlake modulesFromList;
 
         # DO NOT USE - subject to change without notice
         internal = internal-functions;
